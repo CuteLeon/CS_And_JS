778a82669d99d6f710e7027374a3bca86bb90552
Leon <leon.id@qq.com>
2020-01-16 10:42:08 +0800

    修改弹出式链接的行为：在当前浏览器加载；

---
+++ WPFCefSharpDemo/CustomeLifeSpanHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CefSharp;
+
+namespace WPFCefSharpDemo
+{
+    /// <summary>
+    /// 自定义生命周期处理器
+    /// </summary>
+    /// <remarks> 控制链接弹出方式 </remarks>
+    public class CustomeLifeSpanHandler : ILifeSpanHandler
+    {
+        public bool DoClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            return false;
+        }
+
+        public void OnAfterCreated(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+        }
+
+        public void OnBeforeClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+        }
+
+        public bool OnBeforePopup(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
+        {
+            newBrowser = null;
+            chromiumWebBrowser.Load(targetUrl);
+            return true;
+        }
+    }
+}
